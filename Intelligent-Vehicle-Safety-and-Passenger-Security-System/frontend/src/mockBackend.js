2f68c24e353238d3d108ca59ac540ad8e5458d51
Mounika1465 <mouniinumula@gmail.com>
2026-09-22 13:38:50 +0530

    Organize project files into root directory

--- Intelligent-Vehicle-Safety-and-Passenger-Security-System/frontend/src/mockBackend.js
+++
@@ -1,6 +0,0 @@
-import { getTrip, getTrips } from './api/trips'
-import { subscribeLive } from './services/liveTelemetry'
-
-export { getTrips, getTrip, subscribeLive }
-
-export default { getTrips, getTrip, subscribeLive }
